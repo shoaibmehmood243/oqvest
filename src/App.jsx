d48acba329c554b7156e77d40ded5a06dadb8d1a
Shoaib Mehmood <shoaib.m@emblemtechnologies.com>
2023-03-24 16:20:40 +0500

    push request

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './components/common/Header';
 import Banner from './components/common/Banner';
-import Footer from './components/common/Footer';
+import Footer from './components/common/Footer'; 
 
 function App() {
   return (
