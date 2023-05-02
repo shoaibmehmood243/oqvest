e86e835f0234766b467a02c7070b6bcbaf595191
shoaibmehmood243 <shoaibmehmood065@gmail.com>
2023-05-02 22:58:50 +0500

    fixed apply

--- src/components/common/Apply.jsx
+++ src/components/common/Apply.jsx
@@ -5,7 +5,7 @@ const Apply = ()=> {
     return(
         <div className={styles.section}>
             <div className='flex flex-column md:flex-row align-items-center justify-content-around'>
-                <p>Call us today at <strong>(813) 928-9610</strong> for more information </p>
+                <p>Call us today at <strong>(551) 225-0733</strong> for more information </p>
                 <a target='_blank' href={'https://lead.oqvest.com/'}>
                 <Button label='Apply Today' />
                 </a>
